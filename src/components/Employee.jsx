3dd9fca28840dbb813ef014bfb1acf08b1180180
aditya banerjee <adityabanerjee1406@gmail.com>
2024-05-12 16:14:13 +0530

    [EMSUI-#0001] changes in EMS update of employee

--- src/components/Employee.jsx
+++ src/components/Employee.jsx
@@ -5,7 +5,7 @@ export const Employee = ({employee,deleteEmployee}) => {
     const navigate = useNavigate();
     const editEmployee = (e,id) =>{
         navigate(`/updateEmployee/${id}`);
-        e.preventDeafault();
+        e.preventDefault();
 
     }
   return (
@@ -20,7 +20,7 @@ export const Employee = ({employee,deleteEmployee}) => {
             <div className='text-sm text-gray-500'>{employee.emailId}</div>
         </td>
         <td className='text-right px-6 py-4 whitespace-nowrap font-medium text-sm'>
-            <a onClick={(e,id) => editEmployee(e,id)} className='text-indigo-600 hover:text-indigo-800 px-4 hover:cursor-pointer'>Edit</a>
+            <a onClick={(e,id) => editEmployee(e,employee.id)} className='text-indigo-600 hover:text-indigo-800 px-4 hover:cursor-pointer'>Edit</a>
             <a href="#" className='text-indigo-600 hover:text-indigo-800 hover:cursor-pointer' onClick={(e,id)=>deleteEmployee(e,employee.id)}>Delete</a>
         </td>
     </tr>
--- src/components/Updateemployee.jsx
+++ src/components/Updateemployee.jsx
@@ -1,36 +1,83 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import EmployeeService from '../services/EmployeeService'
+import { useParams } from 'react-router-dom';
 
 export const Updateemployee = () => {
-  return (
+
+    const[loading, setLoading]=useState(true);
+    const[employeefname, setEmployeefname]=useState(null);
+    const[employeelname, setEmployeelname]=useState(null);
+    const[employeeemail, setEmployeeemail]=useState(null);
+    const params = useParams();
+
+    useEffect(()=>{
+        const fetchData = async ()=>{
+            setLoading(true);
+            try {
+                const res  = await EmployeeService.getEmployeeById(params.id);
+                console.log(res);
+                setEmployeefname(res.data.firstName);
+                setEmployeelname(res.data.lastName);
+                setEmployeeemail(res.data.emailId);
+                
+            } catch (error) {
+                console.log(error)
+            }
+            setLoading(false);
+        }
+        fetchData();
+    },[])
+    function handleChange(e){
+        const targetName = e.target.name;
+        const targetValue = e.target.value;
+        if(targetName==='firstName'){
+            setEmployeefname(targetValue);
+        } else if (targetName==='lastName'){
+            setEmployeelname(targetValue);
+        } else if (targetName === 'emailId'){
+            setEmployeeemail(targetValue);
+        }
+    }
+
+    function updateEmployee(){
+
+    }
+
+    function reset(){
+
+    }
+  return !loading ? (
     <div className='flex max-w-2xl mx-auto shabdow border-b'>
-    <div className='px-8 py-8'>
-        <div className='font-thin text-2xl tracking-wider'>
-            <h1>Update Employee</h1>
-        </div>
-        <div className='items-center justify-center h-14 w-full my-4'>
-            <label className='block text-gray-600 text-sm font-normal'>
-                First Name
-            </label>
-            <input type='text' name='firstName' value={employee.firstName} onChange={(e)=> handleChange(e)} className='h-10 w-96 border mt-2 px-2 py-2'></input>
-        </div>
-        <div className='items-center justify-center h-14 w-full my-4'>
-            <label className='block text-gray-600 text-sm font-normal'>
-                Last Name
-            </label>
-            <input type='text' name='lastName' value={employee.lastName} onChange={(e)=> handleChange(e)} className='h-10 w-96 border mt-2 px-2 py-2'></input>
-        </div>
-        <div className='items-center justify-center h-14 w-full my-4'>
-            <label className='block text-gray-600 text-sm font-normal'>
-                Email
-            </label>
-            <input type='email' name='emailId' value={employee.emailId} onChange={(e)=> handleChange(e)} className='h-10 w-96 border mt-2 px-2 py-2'></input>
-        </div>
-        <div className='items-center justify-center h-14 w-full my-4 space-x-4 pt-4'>
-            <button className='rounded text-white font-semibold bg-green-400 hover:bg-green-700 py-6 px-6' onClick={updateEmployee}>Update</button>
-            <button className='rounded text-white font-semibold bg-red-400 hover:bg-red-700 py-6 px-6' onClick={reset}>Cancel</button>
-        </div>
-    </div>
-    
+  <div className='px-8 py-8'>
+      <div className='font-thin text-2xl tracking-wider'>
+          <h1>Update Employee</h1>
+      </div>
+      <div className='items-center justify-center h-14 w-full my-4'>
+          <label className='block text-gray-600 text-sm font-normal'>
+              First Name
+          </label>
+          <input type='text' name='firstName' value={employeefname} onChange={(e)=> handleChange(e)} className='h-10 w-96 border mt-2 px-2 py-2'></input>
+      </div>
+      <div className='items-center justify-center h-14 w-full my-4'>
+          <label className='block text-gray-600 text-sm font-normal'>
+              Last Name
+          </label>
+          <input type='text' name='lastName' value={employeelname} onChange={(e)=> handleChange(e)} className='h-10 w-96 border mt-2 px-2 py-2'></input>
+      </div>
+      <div className='items-center justify-center h-14 w-full my-4'>
+          <label className='block text-gray-600 text-sm font-normal'>
+              Email
+          </label>
+          <input type='email' name='emailId' value={employeeemail} onChange={(e)=> handleChange(e)} className='h-10 w-96 border mt-2 px-2 py-2'></input>
+      </div>
+      <div className='items-center justify-center h-14 w-full my-4 space-x-4 pt-4'>
+          <button className='rounded text-white font-semibold bg-green-400 hover:bg-green-700 py-6 px-6' data-id={params.id} onClick={updateEmployee}>Update</button>
+          <button className='rounded text-white font-semibold bg-red-400 hover:bg-red-700 py-6 px-6' onClick={reset}>Cancel</button>
+      </div>
+  </div>
+  
 </div>
+  ):(
+    <div>Loading....</div>
   )
 }
